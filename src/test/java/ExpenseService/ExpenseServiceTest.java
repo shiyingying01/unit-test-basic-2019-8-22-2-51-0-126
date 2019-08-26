62a52da53a228bb3dc610d0066afcd419f3c8f6e
shiyingying01 <718275303@qq.com>
2019-08-26 15:33:39 +0800

    project_is_internal

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -4,6 +4,9 @@ import ExpenseService.Exception.UnexpectedProjectTypeException;
 import ExpenseService.Expense.ExpenseType;
 import ExpenseService.Project.Project;
 import ExpenseService.Project.ProjectType;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,8 +16,13 @@ class ExpenseServiceTest {
     @Test
     void should_return_internal_expense_type_if_project_is_internal() throws UnexpectedProjectTypeException {
         // given
+    	Project project = new Project(ProjectType.INTERNAL, "internalProject");
+    	
         // when
+    	ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
+    	
         // then
+    	assertEquals(ExpenseType.INTERNAL_PROJECT_EXPENSE, expenseType);
     }
 
     @Test
