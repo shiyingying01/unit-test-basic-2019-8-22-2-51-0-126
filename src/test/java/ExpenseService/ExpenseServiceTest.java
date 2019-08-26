c76cfd9bac223e753a40775a2471f9f3eba89a8a
shiyingying01 <718275303@qq.com>
2019-08-26 16:00:26 +0800

    throw_unexpected_project_exception

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -41,22 +41,47 @@ class ExpenseServiceTest {
 
     @Test
     void should_return_expense_type_B_if_project_is_external_and_name_is_project_B() throws UnexpectedProjectTypeException {
-        // given
+    	// given
+    	Project project = new Project(ProjectType.EXTERNAL, "Project B");
+ 	
         // when
+    	ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
+ 	
+    	
         // then
+    	assertEquals(ExpenseType.EXPENSE_TYPE_B, expenseType);
+    	
+    	
     }
 
     @Test
     void should_return_other_expense_type_if_project_is_external_and_has_other_name() throws UnexpectedProjectTypeException {
-        // given
+    	// given
+    	Project project = new Project(ProjectType.EXTERNAL, "otherExpense");
+ 	
         // when
+    	ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
+ 	
+    	
         // then
+    	assertEquals(ExpenseType.OTHER_EXPENSE, expenseType);
     }
 
     @Test
     void should_throw_unexpected_project_exception_if_project_is_invalid() {
-        // given
+    	// given
+    	Project project = new Project(ProjectType.UNEXPECTED_PROJECT_TYPE, "otherType");
+ 	
         // when
+    	String error = "";
+    	try {
+			ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
+		} catch (UnexpectedProjectTypeException e) {
+			// TODO Auto-generated catch block
+			error = e.getMessage();
+		}
+    	
         // then
+    	assertEquals("You enter invalid project type", error);
     }
 }
